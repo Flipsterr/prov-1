cdb294fc6d6a2bd8b818d8e869194c42cd37700e
unknown <filip.von.bromssen@gmail.comm>
2020-10-19 13:47:02 +0200

    Uppgift 3

--- 2/script.js
+++ 2/script.js
@@ -1,5 +1,3 @@
 function sum(number1, number2, number3){
     return number1+number2+number3
 }
-
-console.log(sum(1,2,3))
---
+++ 3/script.js
@@ -0,0 +1,5 @@
+const arr = ["Hej", "på", "dig", "!", "!"]
+
+arr [0] = 1
+arr [arr.length -1] = 2
+
